80d12457b01e9c2acbcec68846dbcfd6203fe20d
p-sgarcia <p-sgarcia@P-SGARCIA.neoris.cxnetworks.net>
2023-03-27 17:53:45 +0200

    commit

---
+++ EjerciciosCurso/src/com/rf/ejercicios/Datos.java
@@ -0,0 +1,23 @@
+package com.rf.ejercicios;
+
+public class Datos {
+	
+	public static void main(String[] args) {
+		int dia = 10;
+		long nDni = 57443222;
+		double precio;
+		final boolean ACTIVO = true;
+		
+		precio = 135.12;
+		
+		System.out.println("Dia/t" + dia);
+		System.out.println("DNI/t" + nDni);
+		System.out.println("Precio/t" + precio);
+		System.out.println("Activo?/t" + ACTIVO);
+
+
+	}
+	
+	
+	
+}
